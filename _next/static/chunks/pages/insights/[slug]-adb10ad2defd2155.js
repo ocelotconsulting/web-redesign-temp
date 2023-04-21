a3e81126998f1d361685f3af815d50876eff39e8
Patrick Byrne <patrick.byrne@ocelotconsulting.com>
2023-04-21 13:08:19 -0500

    Latest updates from Fluid22

--- _next/static/chunks/pages/insights/[slug]-adb10ad2defd2155.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[742],{8433:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/insights/[slug]",function(){return t(1561)}])},1561:function(n,u,t){"use strict";function i(){}t.r(u),t.d(u,{default:function(){return i}})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8433)}),_N_E=n.O()}]);
